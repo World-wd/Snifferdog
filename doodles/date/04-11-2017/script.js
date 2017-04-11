39610db04cebc71cc01a5dd3936c7786d0d50a75
Plasmmer <plasmmer@outlook.com>
2017-04-11 01:04:03 -0300

    #1 First doodle images and script

---
+++ doodles/date/04-11-2017/script.js
@@ -0,0 +1,4 @@
+function doodle() {
+document.getElementsByTagName("body").innerHTML+= "<style>#mainlogo{background-image:url(default.png)}.logo{background-image:url(headerlogo.png);background-size:233px;background-position:-15px -2px}</style>";
+document.getElementsByClassName('logo')[0].setAttribute("title", "yolo")
+}
